00e9ab744cd48db2dc92e3cfc288d461a6d4e03c
Mateus Machado <mateusmmachado2@gmail.com>
2022-01-16 01:27:18 -0300

    Refatorando

---
+++ LocalizaEndereco.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using LocalizaEndereco.Service;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Informe o CEP (apenas números)");
+        var cep = Console.ReadLine();
+
+        var localizaEnderecoService = new LocalizaEnderecoService();
+
+        try
+        {
+            var resultado = await localizaEnderecoService.GetEndereco(cep);
+
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Endereço: " + resultado);
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+    }
+}
---
+++ LocalizaEndereco.Service/LocalizaEnderecoService.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace LocalizaEndereco.Service
+{
+    public class LocalizaEnderecoService
+    {
+        public string ClientUrl { get; set; }
+        public string ClientUrlRequest { get; set; }
+        public string ResponseFormat { get; set; }
+
+        public LocalizaEnderecoService()
+        {
+            ClientUrl = "http://viacep.com.br/";
+            ClientUrlRequest = "ws/";
+            ResponseFormat = "/json/";
+        }
+
+        public async Task<Endereco> GetEndereco(string cep)
+        {
+            var client = new RestClient(ClientUrl);
+
+            cep = cep.Trim().Replace(".", string.Empty).Replace("-", string.Empty);
+
+            var requestUrl = ClientUrlRequest + cep + ResponseFormat;
+
+            var request = new RestRequest(requestUrl, Method.Get);
+
+            //request.AddHeader("Connection", "close");
+
+            var response = await client.ExecuteGetAsync(request);
+
+            var jObject = JObject.Parse(response.Content);
+
+            var endereco = new Endereco();
+
+            if (jObject == null)
+            {
+                return null;
+            }
+
+            JToken? token;
+
+            endereco.CEP = jObject.TryGetValue("cep", out token) ? token.ToString() : null;
+            endereco.Logradouro = jObject.TryGetValue("logradouro", out token) ? token.ToString() : null;
+            endereco.Complemento = jObject.TryGetValue("complemento", out token) ? token.ToString() : null;
+            endereco.Bairro = jObject.TryGetValue("bairro", out token) ? token.ToString() : null;
+            endereco.Cidade = jObject.TryGetValue("localidade", out token) ? token.ToString() : null;
+            endereco.UF = jObject.TryGetValue("uf", out token) ? token.ToString() : null;
+
+            return endereco;
+        }
+    }
+}
