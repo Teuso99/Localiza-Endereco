9dac2dac6dd7379b7b4c2e44d1679cf7f40ea21b
Mateus Machado <mateusmmachado2@gmail.com>
2022-01-16 01:02:09 -0300

    Refatorando

--- Program.cs
+++ Program.cs
@@ -1,37 +1,31 @@
-﻿using Newtonsoft.Json.Linq;
-using RestSharp;
+﻿using LocalizaEnderecoService;
 using System;
-using System.Web;
+using System.Threading.Tasks;
 
 namespace LocalizaEndereco
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var client = new RestClient("https://buscacepinter.correios.com.br");
-
             Console.WriteLine("Informe o CEP (apenas números)");
             var cep = Console.ReadLine();
 
-            string param = "endereco=" + cep + "&tipoCEP=ALL";
+            var localizaEnderecoService = new LocalizaEnderecoService.LocalizaEnderecoService();
 
-            var request = new RestRequest("/app/endereco/carrega-cep-endereco.php?" + param, DataFormat.Json);
+            try
+            {
+                var resultado = await localizaEnderecoService.GetEndereco(cep);
 
-            var response = client.Get(request);
-
-            var jObject = JObject.Parse(response.Content);
-
-            string rua = (string)jObject.SelectToken("dados[0].logradouroDNEC");
-            string bairro = (string)jObject.SelectToken("dados[0].bairro");
-            string cidade = (string)jObject.SelectToken("dados[0].localidade");
-            string estado = (string)jObject.SelectToken("dados[0].uf");
-
-            string resultado = rua + ", " + bairro + ", " + cidade + ", " + estado;
-
-            Console.WriteLine("------------------------------------------------------------");
-            Console.WriteLine("Endereço: " + resultado);
-            Console.WriteLine("------------------------------------------------------------");
+                Console.WriteLine("------------------------------------------------------------");
+                Console.WriteLine("Endereço: " + resultado);
+                Console.WriteLine("------------------------------------------------------------");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
         }
     }
 }
